954f4733b8e4cbeb1d6406ed549ad4ba2a060ef0
Richard Horrocks <richardjhorrocks@gmail.com>
2020-06-01 16:32:23 +0100

    Update eth_etherscan_fork_lengths.py

--- eth_etherscan_fork_lengths.py
+++ eth_etherscan_fork_lengths.py
@@ -1,41 +1,46 @@
 import requests
+import cfscrape
 from bs4 import BeautifulSoup
 from collections import OrderedDict
 
+
 URL = "https://etherscan.io/blocks_forked?p="
 
-def getData(sess, page):
+
+def getData(scraper, page):
     url = URL + page
     print("Retrieving page", page)
-    return BeautifulSoup(sess.get(url).text, 'html.parser')
+    return BeautifulSoup(scraper.get(url).content, 'html.parser')
 
-def getPage(sess, page):
-    table = getData(sess, str(page)).find('table')
+
+def getPage(scraper, page):
+    table = getData(scraper, str(page)).find('table')
     return [[X.text.strip() for X in row.find_all('td')] for row in table.find_all('tr')]
 
+
 def main():
-    resp = requests.get(URL)
-    sess = requests.Session()
-    
+    #session = requests.Session()
+    scraper = cfscrape.create_scraper()
     counts = OrderedDict()
 
     page = 0
-    while True:
+    while page < 1000:
         page += 1
-        data = getPage(sess, page)
-        
+        data = getPage(scraper, page)
+
         try:
             for item in data:
                 if len(item) != 0:
-                    if item[8] in counts:
-                        counts[item[8]] += 1
+                    if item[9] in counts:
+                        counts[item[9]] += 1
                     else:
-                        counts[item[8]] = 1
+                        counts[item[9]] = 1
         except:
             break
 
     for k, v in counts.items():
         print("Forks of length {}: {}".format(k, v))
+
 
 if __name__ == "__main__":
     main()
